6c96ab83564d739c19c6653b198a9f7545b30a71
quaaalud <dludwins@outlook.com>
2023-04-07 21:56:06 -0500

    added argsparse

--- twint_api.py
+++ twint_api.py
@@ -7,11 +7,13 @@ Created on Fri Apr  7 09:45:15 2023
 """
 
 import json
+import argparse
 import subprocess
 import pandas as pd
 from os import chdir
 from pathlib import Path
 
+TWINT_API_DIR = Path(__file__).parent
 
 def create_query(*args):
     return ' '.join(*args)
@@ -19,10 +21,9 @@ def create_query(*args):
 
 def return_query_results(
         query: str,
-        instance: str,
-        save_format='csv'):
-    working_dir = Path(__file__).parent
-    chdir(working_dir)
+        instance: str):
+    global TWINT_API_DIR
+    chdir(TWINT_API_DIR)
     cmd = [
         'go',
         'run',
@@ -30,10 +31,8 @@ def return_query_results(
         '-Query',
         query,
         '-Instance',
-        instance,
-        "-Format",
-        save_format
-    ]
+        'birdsite.xanny.family',  
+    ]  # If Instance needs to be modified use a value from https://github.com/zedeus/nitter/wiki/Instances
 
     process = subprocess.run(
         cmd, 
@@ -52,22 +51,39 @@ def parse_json_returned(json_str: str) -> json:
 
 
 if __name__ == '__main__':
-    save_path = ''  # will save in cwd if not updated
-    save_name = 'twitter_df_test.csv'  # change this name for testing
-    query = [
-        'from:LetMOPLay', 
-        'within_time:10h',
-        # add more query parameters here, order doesn't matter
-    ]
-    instance = 'birdsite.xanny.family'  #  Don't change, this one works lol
-    json_str = return_query_results(
-        create_query(query), 
-        instance, 
-        save_format='json'
+    from datetime import datetime
+    parser = argparse.ArgumentParser(
+        description='Run "Twint Zero" API Twitter Query'
     )
-    response_json = parse_json_returned(json_str)
-    
-    df= pd.read_json(response_json, lines=True)
-    df.to_csv(save_name)
-    print(df)
-    
+    parser.add_argument(
+        'query', 
+        nargs='+', 
+        help='Twitter query string: https://github.com/igorbrigadir/twitter-advanced-search'
+    )
+    args = parser.parse_args()
+    query_str = create_query(
+        args.query
+    )
+    json_str = return_query_results(
+        query_str, 
+        args.instance, 
+        args.save_format
+    )
+    response_json = parse_json_returned(
+        json_str
+    )
+    df = pd.read_json(
+        response_json, 
+        lines=True
+    )
+    save_name = f'{datetime.now()}_{args.instance.replace(".", "_")}.csv'
+    df.to_csv(
+        Path(
+            TWINT_API_DIR, 
+            save_name
+        )
+    )
+    for col in df.columns:
+        print(df[col])
+        print()
+   
